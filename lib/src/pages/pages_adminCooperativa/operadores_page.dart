f24ed85b3a46456523033148c101d0b9f86546fe
Eduardo Olivo <keolivor@utn.edu.ec>
2021-06-28 11:48:20 -0500

    Se implemento información de prueba en la vista de operadores_page

--- lib/src/pages/pages_adminCooperativa/operadores_page.dart
+++ lib/src/pages/pages_adminCooperativa/operadores_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:viaje_express_flutter/src/bloc/bloc_adminCooperativa/adminCooperativa_bloc.dart';
 import 'package:viaje_express_flutter/src/bloc/provider.dart';
 import 'package:viaje_express_flutter/src/utils/colors.dart';
 import 'package:viaje_express_flutter/src/utils/list_rutas.dart';
@@ -27,9 +28,56 @@ class _OperadoresState extends State<Operadores> {
         backgroundColor: yellowLightColors,
         title: Text('Administrador de cooperativa'),
       ),
+      body: _crearListado(adminCoopBloc),
+      floatingActionButton: _crearBoton(context),
     );
   }
 
+
+  Widget _crearListado(AdminCooperativaBloc operadorBloc) {
+    return StreamBuilder(
+      stream: operadorBloc.operadoresStream,
+      builder: (BuildContext context,
+          AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+        if (snapshot.hasData) {
+          final operadorCooperativa = snapshot.data;
+          return ListView.builder(
+              itemCount: operadorCooperativa?.length,
+              itemBuilder: (context, i) =>
+                  _crearItem(context, operadorCooperativa![i]));
+        } else {
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+
+ Widget _crearItem(BuildContext context,
+      Map<String, dynamic> data) {
+    return Dismissible(
+        key: UniqueKey(),
+        background: Container(
+          color: Colors.red,
+        ),
+        onDismissed: (direccion) async {
+          setState(() {});
+        },
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text('${data['id']} - ${data['cedula']}'),
+                subtitle: Text('${data['nombre']} - ${data['apellido']}'),
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  'crudOperador', /* arguments: data */
+                )/* .then((value) => setState(() {})), */
+              )
+            ],
+          ),
+        ));
+  }
 
 
 
